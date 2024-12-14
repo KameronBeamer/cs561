2a1824b72cd2342ddc572dbe97344c7b554958ea
KameronBeamer <beamerkameron@gmail.com>
2024-12-13 21:19:43 -0800

    one

--- OSUCS461/Models/__init__.py
+++ OSUCS461/Models/__init__.py
@@ -4,3 +4,15 @@ class BasePydanticModel(BaseModel):
 	class Config:
 		from_attributes = False
 		validate_assignment = True
+
+class user(BaseModel):
+	uuid: str
+	name: str
+	time_created: int
+
+class UserPost(BaseModel):
+	uuid: str
+	user_uuid: str
+	post_9char: str
+	text: str
+	time_created: int
